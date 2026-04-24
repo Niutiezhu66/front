a0f16a957afad1ce8430e4995ac1c4c8c246a820
牛铁柱 <141615172+Niutiezhu66@users.noreply.github.com>
2026-04-25 01:47:44 +0800

    Delete dist directory

--- dist/assets/logo-08c4762c.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/logo-73e0a506.svg";export{s as _};
--- dist/assets/paper-a1a576fe.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./request-1fa12eb9.js";function a(r){return e({url:`/api/papers/${r}`,method:"get"})}const p=r=>e({url:"/api/papers/list",method:"get",params:r});export{a,p as g};
